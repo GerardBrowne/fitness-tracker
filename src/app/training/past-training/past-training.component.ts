64a470a0707239fb2a0ff098f0dd6b7142955f3f
Gerard Browne <gbrowne@storm.ie>
2018-08-08 14:05:46 +0100

    Completed DataTable, prepped for server connection

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
 @Component({
-  selector: 'app-past-training',
-  templateUrl: './past-training.component.html',
-  styleUrls: ['./past-training.component.css']
+	selector: 'app-past-training',
+	templateUrl: './past-training.component.html',
+	styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
 	displayedColumns = [
 		'date',
 		'name',
@@ -18,10 +18,21 @@ export class PastTrainingComponent implements OnInit {
 	];
 	dataSource = new MatTableDataSource<Exercise>();
 
-  constructor(private trainingService: TrainingService) {}
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngOnInit() {
-	  this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
-  }
+	constructor(private trainingService: TrainingService) { }
 
+	ngOnInit() {
+		this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+	}
+
+	ngAfterViewInit() {
+		this.dataSource.sort = this.sort;
+		this.dataSource.paginator = this.paginator;
+	}
+
+	doFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+	}
 }
